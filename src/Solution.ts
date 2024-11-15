f6b083492e45a9bca181cab71183f2920ef46cdb
norbert-gemes <gemes.norbert@students.jedlik.eu>
2024-11-15 12:02:24 +0100

    adatok beolvasása kész

---
+++ src/Solution.ts
@@ -0,0 +1,18 @@
+import fs from "fs"
+import Candidate from "./Candidate";
+
+export default class Solution{
+    #candidates: Candidate[] = [];
+
+    
+
+    constructor(source: string){
+        fs.readFileSync(source)
+        .toString()
+        .split("\n")
+        .forEach(row => {
+            const actualRow: string = row.trim();
+            this.#candidates.push(new Candidate(actualRow));
+        })
+    }
+}
